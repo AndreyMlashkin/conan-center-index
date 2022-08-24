f92ed4107e68b5ea7ed8c3a01628e5f09ab23b2b
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 18:11:37 +0200

    migrate gainput to conan v2

--- recipes/gainput/all/conanfile.py
+++ recipes/gainput/all/conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile, tools
+from conans import CMake
 import functools
 
 required_conan_version = ">=1.33.0"
@@ -46,7 +47,7 @@ class GainputConan(ConanFile):
             self.requires("xorg/system")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        tools.files.get(self, **self.conan_data["sources"][self.version],
                   destination=self._source_subfolder, strip_root=True)
 
     @functools.lru_cache(1)
@@ -61,7 +62,7 @@ class GainputConan(ConanFile):
 
     def build(self):
         for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+            tools.files.patch(self, **patch)
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -78,7 +79,7 @@ class GainputConan(ConanFile):
             self.cpp_info.system_libs.extend(["xinput", "ws2_32"])
         elif self.settings.os == "Android":
             self.cpp_info.system_libs.extend(["native_app_glue", "log", "android"])
-        elif tools.is_apple_os(self.settings.os):
+        elif tools.apple.is_apple_os(self):
             self.cpp_info.frameworks.extend(["Foundation", "IOKit", "GameController"])
             if self.settings.os == "iOS":
                 self.cpp_info.frameworks.extend(["UIKit", "CoreMotion"])
