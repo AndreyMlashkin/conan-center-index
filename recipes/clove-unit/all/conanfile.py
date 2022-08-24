7e9f2f64ef311f7e1c07d561decdea4a2c59528f
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 15:29:13 +0200

    migrate clove-unit to conan v2

--- recipes/clove-unit/all/conanfile.py
+++ recipes/clove-unit/all/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, tools
+from conan import ConanFile, tools
 
 required_conan_version = ">=1.43.0"
 
@@ -16,7 +16,7 @@ class CloveUnitConan(ConanFile):
         return "source_subfolder"
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        tools.files.get(self, **self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
     def package(self):
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
