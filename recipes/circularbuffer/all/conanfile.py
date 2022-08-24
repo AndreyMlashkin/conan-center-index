9af0da7f3bd601e313d761856ced57311ea404c1
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 17:11:33 +0200

    migrate circularbuffer to conan v2

--- recipes/circularbuffer/all/conanfile.py
+++ recipes/circularbuffer/all/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, tools
+from conan import ConanFile, tools
 
 required_conan_version = ">=1.43.0"
 
@@ -19,7 +19,7 @@ class CircularBufferConan(ConanFile):
         self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
+        tools.files.get(self, **self.conan_data["sources"][self.version], destination=self._source_subfolder, strip_root=True)
 
     def package(self):
         self.copy("LICENSE*", "licenses", self._source_subfolder)
