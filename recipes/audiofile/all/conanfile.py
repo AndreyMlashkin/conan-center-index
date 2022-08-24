4f0ada990aa198545077571b12046749f84617df
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 12:35:40 +0200

    migrate audiofile to conan v2

--- recipes/audiofile/all/conanfile.py
+++ recipes/audiofile/all/conanfile.py
@@ -18,15 +18,15 @@ class AudiofileConan(ConanFile):
 
     def configure(self):
         if self.settings.compiler.cppstd:
-            tools.check_min_cppstd(self, 11)
-        if tools.Version(self.version) < "1.1.0":
+            tools.build.check_min_cppstd(self, 11)
+        if tools.scm.Version(self.version) < "1.1.0":
             self.license = "GPL-3.0-or-later"
 
     def package_id(self):
         self.info.header_only()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
+        tools.files.get(self, **self.conan_data["sources"][self.version])
         os.rename("AudioFile-" + self.version, self._source_subfolder)
 
     def package(self):
