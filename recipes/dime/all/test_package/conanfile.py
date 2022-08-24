41f4c899f501600a5b7a6abc83afc0a1ae4806d0
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 15:54:42 +0200

    migrate dime to conan v2

--- recipes/dime/all/test_package/conanfile.py
+++ recipes/dime/all/test_package/conanfile.py
@@ -1,5 +1,6 @@
 import os
-from conans import ConanFile, CMake, tools
+from conan import ConanFile, tools
+from conans import CMake
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -11,7 +12,7 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        if not tools.cross_building(self):
+        if not tools.build.cross_building(self):
             bin_path = os.path.join("bin", "test_package")
             dxf_path = os.path.join(self.source_folder, "testFile_Bug01.dxf")
             self.run("{} {}".format(bin_path, dxf_path), run_environment=True)
