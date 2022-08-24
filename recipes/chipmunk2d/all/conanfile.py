8e9ba4db57cff10856498eb84ee131c4369c9611
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 12:39:52 +0200

    migrate chipmunk2d to conan v2

--- recipes/chipmunk2d/all/conanfile.py
+++ recipes/chipmunk2d/all/conanfile.py
@@ -1,5 +1,6 @@
 import os
-from conans import ConanFile, CMake, tools
+from conan import ConanFile, tools
+from conans import CMake
 
 
 class Chipmunk2DConan(ConanFile):
@@ -39,7 +40,7 @@ class Chipmunk2DConan(ConanFile):
         del self.settings.compiler.cppstd
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
+        tools.files.get(self, **self.conan_data["sources"][self.version])
         extracted_dir = "Chipmunk2D-Chipmunk-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
