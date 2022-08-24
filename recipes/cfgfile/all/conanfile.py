d88e07ee50c06b0451ff3de60383f8d49120c64e
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 12:39:35 +0200

    migrate cfgfile to conan v2

--- recipes/cfgfile/all/conanfile.py
+++ recipes/cfgfile/all/conanfile.py
@@ -1,5 +1,6 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile, tools
+from conans import CMake
+from conan.errors import ConanInvalidConfiguration
 import os
 import textwrap
 
@@ -40,26 +41,26 @@ class CfgfileConan(ConanFile):
         self._cmake = CMake(self)
         self._cmake.definitions["BUILD_EXAMPLES"] = False
         self._cmake.definitions["BUILD_TESTS"] = False
-        if tools.Version(self.version) >= "0.2.9":
+        if tools.scm.Version(self.version) >= "0.2.9":
             self._cmake.definitions["USE_INTERNAL_ARGS_PARSER"] = False
         self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, "14")
+            tools.build.check_min_cppstd(self, "14")
 
         compiler = str(self.settings.compiler)
         if compiler not in self._compilers_minimum_version:
             self.output.warn("Unknown compiler, assuming it supports at least C++14")
             return
 
-        version = tools.Version(self.settings.compiler.version)
+        version = tools.scm.Version(self.settings.compiler.version)
         if version < self._compilers_minimum_version[compiler]:
             raise ConanInvalidConfiguration("cfgfile requires a compiler that supports at least C++14")
 
     def requirements(self):
-        if tools.Version(self.version) >= "0.2.10":
+        if tools.scm.Version(self.version) >= "0.2.10":
             self.requires("args-parser/6.2.0.1")
         elif self.version == "0.2.9.1":
             self.requires("args-parser/6.2.0.1")
@@ -71,7 +72,7 @@ class CfgfileConan(ConanFile):
         cmake.build()
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
+        tools.files.get(self, **self.conan_data["sources"][self.version], strip_root=True, destination=self._source_subfolder)
 
     def package(self):
         self.copy("COPYING", src=self._source_subfolder, dst="licenses")
