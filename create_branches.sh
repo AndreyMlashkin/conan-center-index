13836e605008f4b47da4839cc4296f530fd23a20
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 18:10:51 +0200

    relay on conan return codes

--- create_branches.sh
+++ create_branches.sh
@@ -1,18 +1,22 @@
+git reset master
 cd recipes/
-sucessfull_builds=$(grep -R "(test package): Running test()" | cut -d "/" -f1)
+sucessfull_builds=$(cat built.txt)
 echo $sucessfull_builds
-for recipe_name in ${sucessfull_builds} ; do
-    firstletter=${recipe_name:0:1}
-    if [[ $firstletter < 'c' ]]; then
-        continue
-    fi
-    if [[ $firstletter > 'f' ]]; then
-        continue
-    fi
+for full_recipe_name in ${sucessfull_builds} ; do
+    echo "full_recipe_name is $full_recipe_name"
 
+    firstletter=${recipe_name:0:1}
+    #if [[ $firstletter < 'g' ]]; then
+    #    continue
+    #fi
+    #if [[ $firstletter > 'g' ]]; then
+    #    continue
+    #fi
+    
+    recipe_name=$(echo $full_recipe_name | cut -d "/" -f1)
     echo $recipe_name
     cd ${recipe_name}/
-    branch_name="migrate_${recipe_name}_to_conan_v2_try5"
+    branch_name="migrate_${recipe_name}_to_conan_v2_try7"
     echo $branch_name
     git br $branch_name
     git co $branch_name
@@ -21,4 +25,5 @@ for recipe_name in ${sucessfull_builds} ; do
     git push my HEAD
     cd ..
     git co master
+    sleep 3
 done
--- recipes/create_recipes.sh
+++ recipes/create_recipes.sh
@@ -1,11 +1,14 @@
+rm reports.txt
+rm built.txt
+
 for recipe_name in */ ; do
     cd $recipe_name
     for d in */ ; do 
         firstletter=${recipe_name:0:1}
-        if [[ $firstletter < 'd' ]]; then
+        if [[ $firstletter < 'g' ]]; then
             continue
         fi
-        if [[ $firstletter > 'f' ]]; then
+        if [[ $firstletter > 'g' ]]; then
             continue
         fi
     
@@ -13,9 +16,12 @@ for recipe_name in */ ; do
         version=$(cat conandata.yml | yq .sources | yq keys | yq .[0])
         full_recipe_name=$recipe_name$version@andrei/test
         echo "build $full_recipe_name"
-        conan create . $full_recipe_name --build missing -ks > local_build.txt
-        cat local_build.txt
-        echo "return code is $?"
+        conan create . $full_recipe_name --build missing -ks
+        retVal=$?
+        echo "return code is $retVal for $full_recipe_name"
+        if [ $retVal -eq 0 ]; then
+          echo "$full_recipe_name" >> ../../built.txt
+        fi
         cd ..
     done
     cd ..
