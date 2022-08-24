b670d2ad9cab38d79916fd67d16a66a6dcb15afa
andrei malashkin <malashkin.andrey@gmail.com>
2022-08-24 12:29:48 +0200

    migrate 7zip/ to conan v2

--- recipes/7zip/19.00/conanfile.py
+++ recipes/7zip/19.00/conanfile.py
@@ -1,5 +1,5 @@
 from conans import ConanFile, tools, AutoToolsBuildEnvironment
-from conans.errors import ConanInvalidConfiguration
+from conan.errors import ConanInvalidConfiguration
 import os
 
 required_conan_version = ">=1.33.0"
@@ -41,7 +41,7 @@ class SevenZipConan(ConanFile):
         url = self.conan_data["sources"][self.version]["url"]
         filename = os.path.basename(urlparse(url).path)
         sha256 = self.conan_data["sources"][self.version]["sha256"]
-        tools.download(url, filename)
+        tools.files.download(self, url, filename)
         tools.check_sha256(filename, sha256)
         self._uncompress_7z(filename)
         os.unlink(filename)
@@ -55,7 +55,7 @@ class SevenZipConan(ConanFile):
 
     def _build_msvc(self):
         with tools.vcvars(self.settings):
-            with tools.chdir(os.path.join("CPP", "7zip")):
+            with tools.files.chdir(self, os.path.join("CPP", "7zip")):
                 self.run("nmake /f makefile PLATFORM=%s" % self._msvc_platform)
 
     def _build_autotools(self):
@@ -65,15 +65,15 @@ class SevenZipConan(ConanFile):
         if self.settings.os == "Windows" and self.settings.compiler == "gcc":
             extra_env["IS_MINGW"] = "1"
         with tools.environment_append(extra_env):
-            with tools.chdir(os.path.join("CPP", "7zip", "Bundles", "LzmaCon")):
+            with tools.files.chdir(self, os.path.join("CPP", "7zip", "Bundles", "LzmaCon")):
                 autotools.make(args=["-f", "makefile.gcc"], target="all")
 
     def _patch_sources(self):
         if self.settings.compiler == "Visual Studio":
             fn = os.path.join("CPP", "Build.mak")
             os.chmod(fn, 0o644)
-            tools.replace_in_file(fn, "-MT", "-{}".format(str(self.settings.compiler.runtime)))
-            tools.replace_in_file(fn, "-MD", "-{}".format(str(self.settings.compiler.runtime)))
+            tools.files.replace_in_file(self, fn, "-MT", "-{}".format(str(self.settings.compiler.runtime)))
+            tools.files.replace_in_file(self, fn, "-MD", "-{}".format(str(self.settings.compiler.runtime)))
 
     def build(self):
         self._patch_sources()
