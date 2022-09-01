17bca991c3689cdc8a31eaf8ea9b6f3df3b36435
andrei malashkin <malashkin.andrey@gmail.com>
2022-09-01 10:38:33 +0200

    migrate immer to conan v2

--- recipes/immer/all/conanfile.py
+++ recipes/immer/all/conanfile.py
@@ -1,4 +1,5 @@
-from conans import ConanFile, tools, errors
+from conan import ConanFile, tools
+from conans import errors
 
 required_conan_version = ">=1.33.0"
 
@@ -23,7 +24,7 @@ class ImmerConan(ConanFile):
         return "source_subfolder"
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
+        tools.files.get(self, **self.conan_data["sources"][self.version],
                   strip_root=True,
                   destination=self._source_subfolder)
 
@@ -49,12 +50,12 @@ class ImmerConan(ConanFile):
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, self._minimum_cpp_standard)
+            tools.build.check_min_cppstd(self, self._minimum_cpp_standard)
         min_version = self._minimum_compilers_version.get(str(self.settings.compiler))
         if not min_version:
             self.output.warn("{} recipe lacks information about the {} compiler support.".format(
                 self.name, self.settings.compiler))
         else:
-            if tools.Version(self.settings.compiler.version) < min_version:
+            if tools.scm.Version(self.settings.compiler.version) < min_version:
                 raise errors.ConanInvalidConfiguration("{} requires C++{} support. The current compiler {} {} does not support it.".format(
                     self.name, self._minimum_cpp_standard, self.settings.compiler, self.settings.compiler.version))
