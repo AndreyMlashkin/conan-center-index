eaec2ed0ffa9d5cdaf4df951d13c1eb6dddd3047
andrei malashkin <malashkin.andrey@gmail.com>
2022-09-01 14:50:20 +0200

    migrate jsonformoderncpp to conan v2

--- recipes/jsonformoderncpp/all/conanfile.py
+++ recipes/jsonformoderncpp/all/conanfile.py
@@ -1,4 +1,7 @@
-from conans import ConanFile, tools, CMake
+from conan import ConanFile, tools
+from conan.tools.scm import Version
+from conan.tools import files
+from conans import CMake
 import os
 
 required_conan_version = ">=1.28.0"
@@ -25,7 +28,7 @@ class JsonformoderncppConan(ConanFile):
         return "source_subfolder"
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
+        files.get(self, **self.conan_data["sources"][self.version])
         extracted_dir = "json-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
@@ -44,7 +47,7 @@ class JsonformoderncppConan(ConanFile):
         self.copy(pattern="LICENSE*", dst="licenses", src=self._source_subfolder)
         cmake = self._configure_cmake()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib"))
+        files.rmdir(self, os.path.join(self.package_folder, "lib"))
         try:
             os.remove(os.path.join(self.package_folder, "nlohmann_json.natvis"))
         except FileNotFoundError:
