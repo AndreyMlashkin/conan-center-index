feda85991827cca4f760d5ef7e4bf4fefe9881f1
MALASHKIN Andrei <andrei.malashkin-ext@hexagon.com>
2023-09-13 11:35:06 +0200

    make test work with python 3.5.x

--- recipes/cpython/all/test_package/py3/test_package.c
+++ recipes/cpython/all/test_package/py3/test_package.c
@@ -13,9 +13,7 @@ main(int argc, char *argv[])
     Py_Initialize();
     PyRun_SimpleString("from time import time, ctime\n"
                        "print('Today is', ctime(time()))\n");
-    if (Py_FinalizeEx() < 0) {
-        exit(120);
-    }
+    Py_Finalize();
     PyMem_RawFree(program);
     return 0;
 }
